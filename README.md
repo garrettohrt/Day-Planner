b4709e48d2b8480a17dba2225d06a9fd1e80b2c5
gmun <garrettohrt@gmail.com>
2022-10-03 14:55:50 -0500

    finish js

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Day-Planner
+<a name="readme-top"></a>
+
+<!-- ABOUT THE PROJECT -->
+![_Users_garrettbootcamp_ch4_index html](https://user-images.githubusercontent.com/109110256/192369079-3fa368d7-5d75-4f8f-9ce7-7e38eae83738.png)
+
+## About The Project
+This project was about taking starter code and creating a functioning work day scheduler while using Bootstrap and moment.js.
+
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Clicking on a timeblock allows you to save a task in the input. All inputs are stored in local storage and can be cleared out with the trash button on the bottom. Timeblocks in the past, present, and future are color coded.
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- CONTACT -->
+## Contact
+
+Garrett Ohrt - garrettohrt@gmail.com
+
+Project Link: [https://garrettohrt.github.io/JavaScript-Quiz/](https://garrettohrt.github.io/JavaScript-Quiz/)
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+var current_task_color = '#ff6961';
+var future_task_color = '#77dd77';
+
+var currentDate = moment().format('dddd, MMMM Do');
+$("#currentDay").text(currentDate);
+
+var tasks = JSON.parse(localStorage.getItem("tasks"));
+var timeSlots = $(".to-do").toArray();
+
+// Check to see whether timeblocks are in the past, present, or future.
+function setTimeSlotColors() {
+    var currentTime = moment().format('kk');
+
+    for (var i = 0; i < timeSlots.length; i++) {
+        if (parseInt(timeSlots[i].id) == (currentTime)) {
+            timeSlots[i].style.backgroundColor = current_task_color;
+        } else if (parseInt(timeSlots[i].id) > currentTime) {
+            timeSlots[i].style.backgroundColor = future_task_color;
+        }
+    }
+}
+
+// Load any saved tasks from storage.
+function getStoredTasks() {
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    $(".display-text").each(function (_index, element) {
+        for (var i = 0; i < tasks.length; i++) {
+            if (tasks[i].pID == element.id) {
+                element.textContent = tasks[i].task;
+            }
+        }
+    })
+}
+
+// Keep task input bars hidden while allowing the one you clicked to be visible.
+$(".to-do").on("click", function () {
+
+    $(this).parent().children(".save-button")[0].style.opacity = 1;
+
+    $(".form-control").each(function (_index, element) {
+        element.style.visibility = "hidden";
+    })
+
+    var formZoneID = "#input-form-" + ($(this)[0].id);
+    $(formZoneID)[0].style.visibility = "visible";
+});
+
+
+$(".save-button").on("click", function () {
+    // Return save buttons to .7 opacity after saving task.
+    $(".save-button").each(function (_index, element) {
+        element.style.opacity = (0.7);
+    })
+
+// Allow form input on the timeblocks that are clicked, and save the input.
+    var currentTaskForm = $(this).parent().children(".to-do").children(".form-control")[0];
+
+    var currentP = $(this).parent().children(".to-do").children(".display-text")[0];
+    var taskDescription = currentTaskForm.value;
+
+    currentP.textContent = taskDescription;
+    currentTaskForm.style.visibility = "hidden";
+
+    var currentTask = {
+        "pID": currentP.id,
+        "task": taskDescription
+    }
+    tasks.push(currentTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+});
+
+//Clear tasks from local storage
+$("#remove-tasks").on("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+
+setTimeSlotColors();
+getStoredTasks();
